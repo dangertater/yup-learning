635270d6607d78e46a66cb3ce28b508178f3deb7
dangertater <106702944+dangertater@users.noreply.github.com>
2023-01-01 07:37:12 -0700

    reviewed erics update, added comments describing it, check if they make sense with e

--- my-app/src/Context/AuthContext.tsx
+++ my-app/src/Context/AuthContext.tsx
@@ -1,10 +1,7 @@
 import React, { useState, useContext, useEffect } from "react"
 import app from "../firebase.js"
 import { onAuthStateChanged } from "firebase/auth"
-import { Auth } from "firebase/auth"
-import { FirebaseApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
-
 
 let AuthContext = React.createContext(null)
 
@@ -12,22 +9,26 @@ export let useAuth = () => {
 	useContext(AuthContext)
 }
 
-export default function AuthProvider(props:any) {
-	
-	let [theUser, setTheUser] = useState()
+export default function AuthProvider(props: any) {
+	let [theUser, setTheUser] = useState<string>()
+	//idk what below line does
 	const auth = getAuth(app)
 	useEffect(() => {
 		// 'unsubscribe' is there because the 'app.onAuthStateChange' returns a method that when called it will "unsubscribe the OnAuth event"
 		//https://www.youtube.com/watch?v=PKwu15ldZ7k&t=706s at 20:00
-		//HERE, we need an observer (which i believe keeps track of data being manipulated?)
-		//----
-	let unsubscribe = onAuthStateChanged(Observer,(user:string)=>{
-		setTheUser(user)
-})
-return unsubscribe
+		//below line authorizes the user whenever anything is changed (2nd useEffect arg being blank?)
+		let unsubscribe = onAuthStateChanged(auth, (user) => {
+			
+			if (!user) throw new Error("User Sucks ASss")
+			if (!user.email) throw new Error("User has no email")
+			//if they pass the auth on rerender it will set active user to user.email
+			setTheUser(user.email)
+		})
+		//the video had me do this, idk why really
+		return unsubscribe
 	}, [])
 
-	let value:any = { theUser }
+	let value: any = { theUser }
 	return (
 		<AuthContext.Provider value={value}>{props.children}</AuthContext.Provider>
 	)
